0f060919a090408e3dd269f4bdd42d7af15f3f9b
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2023-12-03 14:24:42 +0200

    Update structure img

--- Radiology Center/Screens/Forms/LogIn.Designer.cs
+++ Radiology Center/Screens/Forms/LogIn.Designer.cs
@@ -40,7 +40,6 @@
             this.Cancle_btn = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.panel_Side.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -50,20 +49,21 @@
             // panel_Side
             // 
             this.panel_Side.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(25)))), ((int)(((byte)(50)))));
-            this.panel_Side.Controls.Add(this.dateTimePicker1);
             this.panel_Side.Controls.Add(this.pictureBox1);
             this.panel_Side.Location = new System.Drawing.Point(-1, -1);
+            this.panel_Side.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel_Side.Name = "panel_Side";
-            this.panel_Side.Size = new System.Drawing.Size(427, 517);
+            this.panel_Side.Size = new System.Drawing.Size(320, 420);
             this.panel_Side.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(25)))), ((int)(((byte)(50)))));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 41);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(423, 432);
+            this.pictureBox1.Size = new System.Drawing.Size(317, 351);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -83,7 +83,7 @@
             this.nightControlBox1.EnableMaximizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
             this.nightControlBox1.EnableMinimizeButton = true;
             this.nightControlBox1.EnableMinimizeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
-            this.nightControlBox1.Location = new System.Drawing.Point(855, 0);
+            this.nightControlBox1.Location = new System.Drawing.Point(640, 0);
             this.nightControlBox1.MaximizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.nightControlBox1.MaximizeHoverForeColor = System.Drawing.Color.White;
             this.nightControlBox1.MinimizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
@@ -99,12 +99,13 @@
             this.User_TBox.Font = new System.Drawing.Font("Tahoma", 11F);
             this.User_TBox.ForeColor = System.Drawing.Color.DimGray;
             this.User_TBox.Image = null;
-            this.User_TBox.Location = new System.Drawing.Point(520, 142);
+            this.User_TBox.Location = new System.Drawing.Point(390, 115);
+            this.User_TBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.User_TBox.MaxLength = 32767;
             this.User_TBox.Multiline = false;
             this.User_TBox.Name = "User_TBox";
             this.User_TBox.ReadOnly = false;
-            this.User_TBox.Size = new System.Drawing.Size(359, 46);
+            this.User_TBox.Size = new System.Drawing.Size(269, 41);
             this.User_TBox.TabIndex = 5;
             this.User_TBox.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
             this.User_TBox.UseSystemPasswordChar = false;
@@ -116,12 +117,13 @@
             this.Pass_TBox.Font = new System.Drawing.Font("Tahoma", 11F);
             this.Pass_TBox.ForeColor = System.Drawing.Color.DimGray;
             this.Pass_TBox.Image = null;
-            this.Pass_TBox.Location = new System.Drawing.Point(520, 248);
+            this.Pass_TBox.Location = new System.Drawing.Point(390, 202);
+            this.Pass_TBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Pass_TBox.MaxLength = 32767;
             this.Pass_TBox.Multiline = false;
             this.Pass_TBox.Name = "Pass_TBox";
             this.Pass_TBox.ReadOnly = false;
-            this.Pass_TBox.Size = new System.Drawing.Size(359, 46);
+            this.Pass_TBox.Size = new System.Drawing.Size(269, 41);
             this.Pass_TBox.TabIndex = 6;
             this.Pass_TBox.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
             this.Pass_TBox.UseSystemPasswordChar = false;
@@ -131,9 +133,10 @@
             this.User_label.AutoSize = true;
             this.User_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.User_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
-            this.User_label.Location = new System.Drawing.Point(516, 115);
+            this.User_label.Location = new System.Drawing.Point(387, 93);
+            this.User_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.User_label.Name = "User_label";
-            this.User_label.Size = new System.Drawing.Size(114, 24);
+            this.User_label.Size = new System.Drawing.Size(93, 18);
             this.User_label.TabIndex = 7;
             this.User_label.Text = "User Name";
             // 
@@ -142,20 +145,23 @@
             this.Pass_Label.AutoSize = true;
             this.Pass_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pass_Label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
-            this.Pass_Label.Location = new System.Drawing.Point(516, 221);
+            this.Pass_Label.Location = new System.Drawing.Point(387, 180);
+            this.Pass_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Pass_Label.Name = "Pass_Label";
-            this.Pass_Label.Size = new System.Drawing.Size(100, 24);
+            this.Pass_Label.Size = new System.Drawing.Size(83, 18);
             this.Pass_Label.TabIndex = 8;
             this.Pass_Label.Text = "Password";
             // 
             // Log_btn
             // 
             this.Log_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.Log_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Log_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Log_btn.ForeColor = System.Drawing.Color.White;
-            this.Log_btn.Location = new System.Drawing.Point(676, 347);
+            this.Log_btn.Location = new System.Drawing.Point(404, 278);
+            this.Log_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Log_btn.Name = "Log_btn";
-            this.Log_btn.Size = new System.Drawing.Size(123, 59);
+            this.Log_btn.Size = new System.Drawing.Size(92, 48);
             this.Log_btn.TabIndex = 9;
             this.Log_btn.Text = "LOGIN";
             this.Log_btn.UseVisualStyleBackColor = false;
@@ -163,11 +169,13 @@
             // Cancle_btn
             // 
             this.Cancle_btn.BackColor = System.Drawing.Color.White;
+            this.Cancle_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Cancle_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Cancle_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
-            this.Cancle_btn.Location = new System.Drawing.Point(520, 347);
+            this.Cancle_btn.Location = new System.Drawing.Point(516, 278);
+            this.Cancle_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Cancle_btn.Name = "Cancle_btn";
-            this.Cancle_btn.Size = new System.Drawing.Size(123, 59);
+            this.Cancle_btn.Size = new System.Drawing.Size(92, 48);
             this.Cancle_btn.TabIndex = 10;
             this.Cancle_btn.Text = "CANCLE";
             this.Cancle_btn.UseVisualStyleBackColor = false;
@@ -175,9 +183,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(474, 128);
+            this.pictureBox2.Location = new System.Drawing.Point(356, 110);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(40, 60);
+            this.pictureBox2.Size = new System.Drawing.Size(30, 43);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
@@ -185,25 +194,19 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(474, 248);
+            this.pictureBox3.Location = new System.Drawing.Point(356, 206);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(40, 46);
+            this.pictureBox3.Size = new System.Drawing.Size(30, 37);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 12;
             this.pictureBox3.TabStop = false;
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(372, 117);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 5;
-            // 
             // LogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(976, 511);
+            this.ClientSize = new System.Drawing.Size(732, 415);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.Cancle_btn);
@@ -215,6 +218,7 @@
             this.Controls.Add(this.nightControlBox1);
             this.Controls.Add(this.panel_Side);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LogIn";
             this.Text = "LogIn";
             this.panel_Side.ResumeLayout(false);
@@ -238,6 +242,5 @@
         private System.Windows.Forms.Button Cancle_btn;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- Radiology Center/Screens/Forms/Profile.cs
+++ Radiology Center/Screens/Forms/Profile.cs
@@ -51,5 +51,10 @@ namespace Radiology_Center.Screens.Forms
 		{
 
 		}
-	}
+
+        private void foxLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
